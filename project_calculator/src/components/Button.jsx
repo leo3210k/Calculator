b657204a91b1ac6f326753258d821dd1eb576560
leonardo <leocoelho.pi@gmail.com>
2022-03-23 20:34:15 -0300

    add equal button style

--- project_calculator/src/components/Button.jsx
+++ project_calculator/src/components/Button.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Button.css';
 
-export default props => 
-    <button onClick={props.click} class="button">{ props.value }</button>
+export default props => {
+    let classes = 'button ' 
+    classes += props.blue ? ' button-blue' : ''
+
+    return <button onClick={props.click} className={classes}>{ props.value }</button>
+}
