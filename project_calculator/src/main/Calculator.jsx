61e7754aa4fe26e3c502e3b36a73f29e838a3d14
Leonardo <admin@DSK-PI-056.piconectado.grupohpar.corp>
2022-03-31 12:53:14 -0300

    update from class state to function state

--- project_calculator/src/main/Calculator.jsx
+++ project_calculator/src/main/Calculator.jsx
@@ -1,75 +1,52 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+import React, { useState } from 'react';
 import './Calculator.css';
 
 import Display from '../components/Display';
 import Button from '../components/Button';
 
-const initialState = {
-    values: [0, 0],
-    display: '0',
-    operation: '',
-    current: 0
-}
+export default function Calculator() {
+    const [values, setValues] = useState([])
+    const [display, setDisplay] = useState('0')
+    const [operation, setOperation] = useState('')
+    const [current, setCurrent] = useState(0)
 
-export default class Calculator extends Component {
-    state = { ...initialState }
-
-    constructor(props) {
-        super(props)
-
-        this.updateDisplay = this.updateDisplay.bind(this)
-    }
-
-    clearDisplay() {
-        console.log('teste')
-    }
-
-    updateDisplay(element) {
+    function updateDisplay(element) {
         const number = element.target.innerHTML
 
-        // if(this.state.display == '0') this.clearDisplay
-        if(this.state.display == '0') {
-            this.setState({ display: number }) 
+        if(display == '0') {
+            setDisplay(number)
         } else {
-            this.setState({ display: this.state.display + number })
+            setDisplay(display + number)
         }
     }
-
-    setOperation(operation) {
-
-    }
     
-    render() {
-        return(
-            <div class="calculator">
-                <Display display={this.state.display}/>
-                <Button value="%" click={this.updateDisplay}/>
-                <Button value="CE" click={this.updateDisplay}/>
-                <Button value="C" click={this.updateDisplay}/>
-                <Button value={<FontAwesomeIcon icon="fa-solid fa-delete-left" />} click={this.updateDisplay}/>
-                <Button value="1/x" font_operation/>
-                <Button value="x²" font_operation/>
-                <Button value="²√x" click={this.clearDisplay}/>
-                <Button value="/" click={this.setOperation}/>
-                <Button value="7" click={this.updateDisplay}/>
-                <Button value="8" click={this.updateDisplay}/>
-                <Button value="9" click={this.updateDisplay}/>
-                <Button value="X" click={this.setOperation}/>
-                <Button value="4" click={this.updateDisplay}/>
-                <Button value="5" click={this.updateDisplay}/>
-                <Button value="6" click={this.updateDisplay}/>
-                <Button value="-" click={this.setOperation}/>
-                <Button value="1" click={this.updateDisplay}/>
-                <Button value="2" click={this.updateDisplay}/>
-                <Button value="3" click={this.updateDisplay}/>
-                <Button value="+" click={this.setOperation}/>
-                <Button value="+" click={this.updateDisplay}/>
-                <Button value="0" click={this.updateDisplay}/>
-                <Button value="," click={this.updateDisplay}/>
-                <Button value="=" click={this.setOperation} blue/>
-            </div>
-        )
-    }
+    return(
+        <div class="calculator">
+            <Display display={display}/>
+            <Button value="%" click={updateDisplay}/>
+            <Button value="CE" click={updateDisplay}/>
+            <Button value="C" click={updateDisplay}/>
+            <Button value="Apagar" click={updateDisplay}/>
+            <Button value="1/x"/>
+            <Button value="x²"/>
+            <Button value="²√x" click={setDisplay('0')}/>
+            <Button value="/" click={setOperation}/>
+            <Button value="7" click={updateDisplay}/>
+            <Button value="8" click={updateDisplay}/>
+            <Button value="9" click={updateDisplay}/>
+            <Button value="X" click={setOperation}/>
+            <Button value="4" click={updateDisplay}/>
+            <Button value="5" click={updateDisplay}/>
+            <Button value="6" click={updateDisplay}/>
+            <Button value="-" click={setOperation}/>
+            <Button value="1" click={updateDisplay}/>
+            <Button value="2" click={updateDisplay}/>
+            <Button value="3" click={updateDisplay}/>
+            <Button value="+" click={setOperation}/>
+            <Button value="+" click={updateDisplay}/>
+            <Button value="0" click={updateDisplay}/>
+            <Button value="," click={updateDisplay}/>
+            <Button value="=" click={setOperation} blue/>
+        </div>
+    )
 }
